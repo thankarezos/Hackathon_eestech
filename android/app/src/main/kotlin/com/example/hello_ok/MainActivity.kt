e3393eb679fef5c3dd0aa64dd8a7b367fa5c33c7
CelaniDe <celanide@gmail.com>
2023-03-19 10:26:42 +0200

    Added authentication with firebase

---
+++ android/app/src/main/kotlin/com/example/hello_ok/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hello_ok
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hello_ok/widget_tree.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WidgetTree(),
+    );
+  }
+}
