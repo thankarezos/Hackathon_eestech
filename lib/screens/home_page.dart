2d2f19d0a545430d32f52bac23d60af3085a2065
CelaniDe <celanide@gmail.com>
2023-03-19 11:42:48 +0200

    Add button send data with function

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:hello_ok/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -23,6 +24,27 @@ class HomePage extends StatelessWidget {
     return ElevatedButton(onPressed: signOut, child: const Text('Sign out'));
   }
 
+  Future<void> sendData() async {
+    // create a reference to the Firebase Realtime Database location
+    DatabaseReference geoPositionRef = FirebaseDatabase.instance
+        .ref()
+        .child('locations')
+        .push(); // generate a unique ID for the new data
+
+// create a Map object that contains the geo position data
+    Map<String, dynamic> geoPositionData = {
+      'onoma': '123',
+      'otinani': '800maou',
+    };
+
+// send the data to Firebase Realtime Database
+    await geoPositionRef.set(geoPositionData);
+  }
+
+  Widget _sendData() {
+    return ElevatedButton(onPressed: sendData, child: const Text('Send data'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +58,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[_userUid(), _signOutButton()],
+          children: <Widget>[_userUid(), _signOutButton(), _sendData()],
         ),
       ),
     );
